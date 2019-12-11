4bf49145957b5804736b862bc0a941e597099f90
iii-ishida <dev.j.ishida@gmail.com>
2019-12-11 22:43:15 +0900

    fix: apply mix format

--- lib/ds_wrapper/key.ex
+++ lib/ds_wrapper/key.ex
@@ -7,18 +7,17 @@ defmodule DsWrapper.Key do
 
   @doc """
     new `GoogleApi.Datastore.V1.Model.Key`
-  
+
     ## Examples
-  
+
         iex> DsWrapper.Key.new("SomeKind")
         %GoogleApi.Datastore.V1.Model.Key{path: [%GoogleApi.Datastore.V1.Model.PathElement{kind: "SomeKind"}]}
 
         iex> DsWrapper.Key.new("SomeKind", "some-name")
         %GoogleApi.Datastore.V1.Model.Key{path: [%GoogleApi.Datastore.V1.Model.PathElement{kind: "SomeKind", name: "some-name"}]}
 
-  """  
+  """
   def new(kind, name \\ nil, parent \\ %Key{path: []}) do
     %Key{path: parent.path ++ [%PathElement{kind: kind, name: name}]}
   end
 end
-
