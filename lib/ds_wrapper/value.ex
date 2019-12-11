a494d5e1605e8f491d196b4944bce28ef77c8a97
iii-ishida <dev.j.ishida@gmail.com>
2019-12-11 18:57:55 +0900

    feat: add wrapper for GoogleApi.Datastore.V1.Model.Value

---
+++ lib/ds_wrapper/value.ex
@@ -0,0 +1,94 @@
+defmodule DsWrapper.Value do
+  @moduledoc """
+  A wrapper for GoogleApi.Datastore.V1.Model.Value
+  """
+
+  alias GoogleApi.Datastore.V1.Model.{ArrayValue, Entity, LatLng, Value}
+
+  @doc """
+  convert to a `GoogleApi.Datastore.V1.Model.Value`
+
+  ## Examples
+
+      iex> DsWrapper.Value.from_native(123)
+      %GoogleApi.Datastore.V1.Model.Value{integerValue: "123"}
+
+      iex> DsWrapper.Value.from_native(123, true)
+      %GoogleApi.Datastore.V1.Model.Value{integerValue: "123", excludeFromIndexes: true}
+
+  """
+  def from_native(value, exclude_from_index \\ nil)
+
+  def from_native(value, exclude_from_index) when is_integer(value) do
+    %Value{integerValue: Integer.to_string(value), excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(value, exclude_from_index) when is_float(value) do
+    %Value{doubleValue: value, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(value, exclude_from_index) when is_bitstring(value) do
+    %Value{stringValue: value, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(value, exclude_from_index) when is_boolean(value) do
+    %Value{booleanValue: value, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(value, exclude_from_index) when is_nil(value) do
+    %Value{nullValue: "NULL_VALUE", excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(value, exclude_from_index) when is_list(value) do
+    values = Enum.map(value, &from_native/1)
+    %Value{arrayValue: %ArrayValue{values: values}, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(%{latitude: lat, longitude: lon}, exclude_from_index) do
+    %Value{geoPointValue: %LatLng{latitude: lat, longitude: lon}, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(%Entity{} = value, exclude_from_index) do
+    %Value{entityValue: value, excludeFromIndexes: exclude_from_index}
+  end
+
+  def from_native(%DateTime{} = value, exclude_from_index) do
+    %Value{timestampValue: value, excludeFromIndexes: exclude_from_index}
+  end
+
+  @doc """
+  convert from a `GoogleApi.Datastore.V1.Model.Value`
+
+  ## Examples
+
+      iex> DsWrapper.Value.to_native(%GoogleApi.Datastore.V1.Model.Value{integerValue: "123"})
+      123
+
+  """
+  def to_native(%Value{integerValue: value}) when not is_nil(value) do
+    value |> Integer.parse() |> elem(0)
+  end
+
+  def to_native(%Value{arrayValue: value}) when not is_nil(value) do
+    Enum.map(value.values, &to_native/1)
+  end
+
+  def to_native(%Value{entityValue: value}) when not is_nil(value) do
+    Enum.reduce(value.properties, %{}, fn {key, value}, acc ->
+      Map.merge(acc, %{key => to_native(value)})
+    end)
+  end
+
+  def to_native(%Value{geoPointValue: %LatLng{latitude: lat, longitude: lon}} = value) when not is_nil(value) do
+    %{latitude: lat, longitude: lon}
+  end
+
+  def to_native(%Value{} = value) do
+    not_nil_value = Map.from_struct(value) |> Enum.find(fn {_, v} -> v != nil end)
+
+    case not_nil_value do
+      {_, v} -> v
+      _ -> nil
+    end
+  end
+end
---
+++ test/ds_wrapper/value_test.exs
@@ -0,0 +1,87 @@
+defmodule DsWrapper.ValueTest do
+  use ExUnit.Case, async: true
+  doctest DsWrapper.Value
+
+  alias GoogleApi.Datastore.V1.Model.{ArrayValue, Entity, Key, LatLng, PathElement, Value}
+
+  describe "from_native/2" do
+    test "integerValue" do
+      assert DsWrapper.Value.from_native(123) == %Value{integerValue: "123"}
+      assert DsWrapper.Value.from_native(123, true) == %Value{integerValue: "123", excludeFromIndexes: true}
+    end
+
+    test "doubleValue" do
+      assert DsWrapper.Value.from_native(12.3) == %Value{doubleValue: 12.3}
+      assert DsWrapper.Value.from_native(12.3, true) == %Value{doubleValue: 12.3, excludeFromIndexes: true}
+    end
+
+    test "stringValue" do
+      assert DsWrapper.Value.from_native("some value") == %Value{stringValue: "some value"}
+      assert DsWrapper.Value.from_native("some value", true) == %Value{stringValue: "some value", excludeFromIndexes: true}
+    end
+
+    test "nullValue" do
+      assert DsWrapper.Value.from_native(nil) == %Value{nullValue: "NULL_VALUE"}
+      assert DsWrapper.Value.from_native(nil, true) == %Value{nullValue: "NULL_VALUE", excludeFromIndexes: true}
+    end
+
+    test "geoPointValue" do
+      assert DsWrapper.Value.from_native(%{latitude: 123, longitude: 456}) == %Value{geoPointValue: %LatLng{latitude: 123, longitude: 456}}
+      assert DsWrapper.Value.from_native(%{latitude: 123, longitude: 456}, true) == %Value{geoPointValue: %LatLng{latitude: 123, longitude: 456}, excludeFromIndexes: true}
+    end
+
+    test "entityValue" do
+      entity = %Entity{
+        key: %Key{path: [%PathElement{kind: "some_kind", name: "some-name"}]},
+        properties: %{"some-property" => %Value{stringValue: "some value"}}
+      }
+
+      assert DsWrapper.Value.from_native(entity) == %Value{entityValue: entity}
+      assert DsWrapper.Value.from_native(entity, true) == %Value{entityValue: entity, excludeFromIndexes: true}
+    end
+
+    test "arrayValue" do
+      assert DsWrapper.Value.from_native([123, "some string"]) == %Value{arrayValue: %ArrayValue{values: [%Value{integerValue: "123"}, %Value{stringValue: "some string"}]}}
+
+      assert DsWrapper.Value.from_native([123, "some string"], true) == %Value{
+               arrayValue: %ArrayValue{values: [%Value{integerValue: "123"}, %Value{stringValue: "some string"}]},
+               excludeFromIndexes: true
+             }
+    end
+  end
+
+  describe "to_native/1" do
+    test "integerValue" do
+      assert DsWrapper.Value.to_native(%Value{integerValue: "123"}) == 123
+    end
+
+    test "doubleValue" do
+      assert DsWrapper.Value.to_native(%Value{doubleValue: 12.3}) == 12.3
+    end
+
+    test "stringValue" do
+      assert DsWrapper.Value.to_native(%Value{stringValue: "some value"}) == "some value"
+    end
+
+    test "nullValue" do
+      assert DsWrapper.Value.to_native(%Value{}) == nil
+    end
+
+    test "geoPointValue" do
+      assert DsWrapper.Value.to_native(%Value{geoPointValue: %LatLng{latitude: 123, longitude: 456}}) == %{latitude: 123, longitude: 456}
+    end
+
+    test "entityValue" do
+      entity = %Entity{
+        key: %Key{path: [%PathElement{kind: "some_kind", name: "some-name"}]},
+        properties: %{"some-property" => %Value{stringValue: "some value"}}
+      }
+
+      assert DsWrapper.Value.to_native(%Value{entityValue: entity}) == %{"some-property" => "some value"}
+    end
+
+    test "arrayValue" do
+      assert DsWrapper.Value.to_native(%Value{arrayValue: %ArrayValue{values: [%Value{integerValue: "123"}, %Value{stringValue: "some string"}]}}) == [123, "some string"]
+    end
+  end
+end
