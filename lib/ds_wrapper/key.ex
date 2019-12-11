705f0c7cf5af597c7d799b92e85ee933c43d6a6d
iii-ishida <dev.j.ishida@gmail.com>
2019-12-11 18:58:49 +0900

    feat: add wrapper for GoogleApi.Datastore.V1.Model.Key

---
+++ lib/ds_wrapper/key.ex
@@ -0,0 +1,24 @@
+defmodule DsWrapper.Key do
+  @moduledoc """
+  A wrapper for `GoogleApi.Datastore.V1.Model.Key`
+  """
+
+  alias GoogleApi.Datastore.V1.Model.{Key, PathElement}
+
+  @doc """
+    new `GoogleApi.Datastore.V1.Model.Key`
+  
+    ## Examples
+  
+        iex> DsWrapper.Key.new("SomeKind")
+        %GoogleApi.Datastore.V1.Model.Key{path: [%GoogleApi.Datastore.V1.Model.PathElement{kind: "SomeKind"}]}
+
+        iex> DsWrapper.Key.new("SomeKind", "some-name")
+        %GoogleApi.Datastore.V1.Model.Key{path: [%GoogleApi.Datastore.V1.Model.PathElement{kind: "SomeKind", name: "some-name"}]}
+
+  """  
+  def new(kind, name \\ nil, parent \\ %Key{path: []}) do
+    %Key{path: parent.path ++ [%PathElement{kind: kind, name: name}]}
+  end
+end
+
---
+++ test/ds_wrapper/key_test.exs
@@ -0,0 +1,27 @@
+defmodule DsWrapper.KeyTest do
+  use ExUnit.Case, async: true
+  doctest DsWrapper.Key
+
+  alias GoogleApi.Datastore.V1.Model.{ArrayValue, Entity, Key, LatLng, PathElement, Value}
+
+  describe "new/3" do
+    test "without parent or name" do
+      assert DsWrapper.Key.new("SomeKind") == %Key{path: [%PathElement{kind: "SomeKind"}]}
+    end
+
+    test "without parent, with name" do
+      assert DsWrapper.Key.new("SomeKind", "some-name") == %Key{path: [%PathElement{kind: "SomeKind", name: "some-name"}]}
+    end
+
+    test "with parent and name" do
+      parent = DsWrapper.Key.new("ParentKind", "parent-name")
+
+      assert DsWrapper.Key.new("SomeKind", "some-name", parent) == %Key{
+               path: [
+                 %PathElement{kind: "ParentKind", name: "parent-name"},
+                 %PathElement{kind: "SomeKind", name: "some-name"}
+               ]
+             }
+    end
+  end
+end
