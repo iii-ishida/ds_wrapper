fcecd8b2ef4a9757894a99384cb573fb95317b8b
iii-ishida <dev.j.ishida@gmail.com>
2019-12-13 20:48:00 +0900

    feat: add wrapper for GoogleApi.Datastore.V1.Model.Query

---
+++ lib/ds_wrapper/query.ex
@@ -0,0 +1,130 @@
+defmodule DsWrapper.Query do
+  @moduledoc """
+  A wrapper for GoogleApi.Datastore.V1.Model.Query
+  """
+
+  alias DsWrapper.Value
+
+  alias GoogleApi.Datastore.V1.Model.{
+    CompositeFilter,
+    Filter,
+    KindExpression,
+    PropertyFilter,
+    PropertyOrder,
+    PropertyReference,
+    Query
+  }
+
+  @doc """
+  new `%GoogleApi.Datastore.V1.Model.Query` for kind
+
+  ## Examples
+
+      iex> DsWrapper.Query.new_query("SomeKind")
+      %GoogleApi.Datastore.V1.Model.Query{...}
+
+  """
+  def new_query(kind) do
+    %Query{kind: %KindExpression{name: kind}}
+  end
+
+  @doc """
+  add a property filter to the query
+
+  ## Examples
+
+      iex> import DsWrapper.Query
+      iex> new_query("SomeKind")
+      ...> |> where("some_property", "=", "some value")
+      %GoogleApi.Datastore.V1.Model.Query{...}
+
+  """
+  def where(%Query{} = query, property, operator, value) do
+    current_composite_filter =
+      case query.filter do
+        %{compositeFilter: composite_filter} -> composite_filter
+        _ -> %CompositeFilter{filters: [], op: "AND"}
+      end
+
+    new_filter = %Filter{
+      propertyFilter: %PropertyFilter{
+        property: new_property_reference(property),
+        op: to_op(operator),
+        value: Value.from_native(value)
+      }
+    }
+
+    new_filters = current_composite_filter.filters ++ [new_filter]
+    new_composite_filter = %CompositeFilter{current_composite_filter | filters: new_filters}
+
+    %Query{query | filter: %Filter{compositeFilter: new_composite_filter}}
+  end
+
+  @doc """
+  sort the results by a property name. By default, an ascending sort order will be used.
+  to sort in descending order, provide a second argument to `:desc`.
+
+  ## Examples
+
+      iex> import DsWrapper.Query
+      iex> new_query("SomeKind")
+      ...> |> order("some_property")
+      %GoogleApi.Datastore.V1.Model.Query{...}
+
+  """
+  def order(%Query{} = query, property, direction \\ nil) do
+    order = [new_property_order(property, to_direction(direction))]
+
+    %Query{query | order: (query.order || []) ++ order}
+  end
+
+  @doc """
+  set a limit on the number of results to be returned.
+
+  ## Examples
+
+      iex> import DsWrapper.Query
+      iex> new_query("SomeKind")
+      ...> |> limit(100)
+      %GoogleApi.Datastore.V1.Model.Query{...}
+
+  """
+  def limit(%Query{} = query, limit) do
+    %Query{query | limit: limit}
+  end
+
+  @doc """
+  set the cursor to start the results at.
+
+  ## Examples
+
+      iex> import DsWrapper.Query
+      iex> new_query("SomeKind")
+      ...> |> start(cursor)
+      %GoogleApi.Datastore.V1.Model.Query{...}
+
+  """
+  def start(%Query{} = query, cursor) do
+    %Query{query | startCursor: cursor}
+  end
+
+  defp new_property_order(property, direction) do
+    %PropertyOrder{
+      property: new_property_reference(property),
+      direction: direction
+    }
+  end
+
+  defp new_property_reference(property) do
+    %PropertyReference{name: property}
+  end
+
+  defp to_op("="), do: "EQUAL"
+  defp to_op("<"), do: "LESS_THAN"
+  defp to_op("<="), do: "LESS_THAN_OR_EQUAL"
+  defp to_op(">"), do: "GREATER_THAN"
+  defp to_op(">="), do: "GREATER_THAN_OR_EQUAL"
+
+  defp to_direction(:desc), do: "DESCENDING"
+  defp to_direction(_), do: "ASCENDING"
+end
---
+++ test/ds_wrapper/query_test.exs
@@ -0,0 +1,168 @@
+defmodule DsWrapper.QueryTest do
+  use ExUnit.Case, async: true
+
+  alias GoogleApi.Datastore.V1.Model.{
+    CompositeFilter,
+    Filter,
+    KindExpression,
+    PropertyFilter,
+    PropertyOrder,
+    PropertyReference,
+    Query,
+    Value
+  }
+
+  @query DsWrapper.Query.new_query("SomeKind")
+
+  test "new_query/1" do
+    assert DsWrapper.Query.new_query("SomeKind") == %Query{
+             kind: %KindExpression{name: "SomeKind"}
+           }
+  end
+
+  describe "where/3" do
+    test "single" do
+      assert DsWrapper.Query.where(@query, "some_property", "=", "some value") == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               filter: %Filter{
+                 compositeFilter: %CompositeFilter{
+                   filters: [
+                     %Filter{
+                       propertyFilter: %PropertyFilter{
+                         property: %PropertyReference{name: "some_property"},
+                         op: "EQUAL",
+                         value: %Value{stringValue: "some value"}
+                       }
+                     }
+                   ],
+                   op: "AND"
+                 }
+               }
+             }
+    end
+
+    test "multiple" do
+      query =
+        @query
+        |> DsWrapper.Query.where("some_property_01", "=", "some value 01")
+        |> DsWrapper.Query.where("some_property_02", "=", "some value 02")
+
+      assert query == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               filter: %Filter{
+                 compositeFilter: %CompositeFilter{
+                   filters: [
+                     %Filter{
+                       propertyFilter: %PropertyFilter{
+                         property: %PropertyReference{name: "some_property_01"},
+                         op: "EQUAL",
+                         value: %Value{stringValue: "some value 01"}
+                       }
+                     },
+                     %Filter{
+                       propertyFilter: %PropertyFilter{
+                         property: %PropertyReference{name: "some_property_02"},
+                         op: "EQUAL",
+                         value: %Value{stringValue: "some value 02"}
+                       }
+                     }
+                   ],
+                   op: "AND"
+                 }
+               }
+             }
+    end
+
+    test "operator is =" do
+      assert_equals_property_filter(DsWrapper.Query.where(@query, "some_property", "=", "some value"), %PropertyFilter{
+        property: %PropertyReference{name: "some_property"},
+        op: "EQUAL",
+        value: %Value{stringValue: "some value"}
+      })
+    end
+
+    test "operator is >" do
+      assert_equals_property_filter(DsWrapper.Query.where(@query, "some_property", ">", "some value"), %PropertyFilter{
+        property: %PropertyReference{name: "some_property"},
+        op: "GREATER_THAN",
+        value: %Value{stringValue: "some value"}
+      })
+    end
+
+    test "operator is >=" do
+      assert_equals_property_filter(DsWrapper.Query.where(@query, "some_property", ">=", "some value"), %PropertyFilter{
+        property: %PropertyReference{name: "some_property"},
+        op: "GREATER_THAN_OR_EQUAL",
+        value: %Value{stringValue: "some value"}
+      })
+    end
+
+    test "operator is <" do
+      assert_equals_property_filter(DsWrapper.Query.where(@query, "some_property", "<", "some value"), %PropertyFilter{
+        property: %PropertyReference{name: "some_property"},
+        op: "LESS_THAN",
+        value: %Value{stringValue: "some value"}
+      })
+    end
+
+    test "operator is <=" do
+      assert_equals_property_filter(DsWrapper.Query.where(@query, "some_property", "<=", "some value"), %PropertyFilter{
+        property: %PropertyReference{name: "some_property"},
+        op: "LESS_THAN_OR_EQUAL",
+        value: %Value{stringValue: "some value"}
+      })
+    end
+  end
+
+  describe "order/3" do
+    test "single" do
+      assert DsWrapper.Query.order(@query, "some_property") == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               order: [%PropertyOrder{property: %PropertyReference{name: "some_property"}, direction: "ASCENDING"}]
+             }
+    end
+
+    test "multiple" do
+      assert DsWrapper.Query.order(@query, "some_property_01") |> DsWrapper.Query.order("some_property_02", :desc) == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               order: [
+                 %PropertyOrder{property: %PropertyReference{name: "some_property_01"}, direction: "ASCENDING"},
+                 %PropertyOrder{property: %PropertyReference{name: "some_property_02"}, direction: "DESCENDING"}
+               ]
+             }
+    end
+
+    test "direction is :desc" do
+      assert DsWrapper.Query.order(@query, "some_property", :desc) == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               order: [%PropertyOrder{property: %PropertyReference{name: "some_property"}, direction: "DESCENDING"}]
+             }
+    end
+
+    test "direction is not :desc" do
+      assert DsWrapper.Query.order(@query, "some_property", :asc) == %Query{
+               kind: %KindExpression{name: "SomeKind"},
+               order: [%PropertyOrder{property: %PropertyReference{name: "some_property"}, direction: "ASCENDING"}]
+             }
+    end
+  end
+
+  test "limit/2" do
+    assert DsWrapper.Query.limit(@query, 100) == %Query{
+             kind: %KindExpression{name: "SomeKind"},
+             limit: 100
+           }
+  end
+
+  test "start/2" do
+    assert DsWrapper.Query.start(@query, "some-cursor") == %Query{
+             kind: %KindExpression{name: "SomeKind"},
+             startCursor: "some-cursor"
+           }
+  end
+
+  defp assert_equals_property_filter(query, property_filter) do
+    filter = query.filter.compositeFilter.filters |> List.first()
+    assert filter.propertyFilter == property_filter
+  end
+end
